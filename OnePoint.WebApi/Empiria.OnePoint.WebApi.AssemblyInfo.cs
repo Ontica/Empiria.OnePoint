704cbe054be5bbb00fd4affc44c2574c885cc827
jmcota <jmcota@ontica.org>
2018-02-03 12:51:48 -0500

    Add web api module

---
+++ OnePoint.WebApi/Empiria.OnePoint.WebApi.AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿/* Empiria OnePoint ******************************************************************************************
+*                                                                                                            *
+*  Solution : Empiria OnePoint                             System  : Web Api                                 *
+*  Assembly : Empiria.OnePoint.WebApi.dll                  Pattern : Assembly Attributes File                *
+*                                                          License : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Web API HTTP/JSON RESTful interface to access Empiria OnePoint services.                       *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTitle("Empiria.OnePoint.WebApi")]
+[assembly: AssemblyDescription("Web API HTTP/JSON RESTful interface to access Empiria OnePoint services.")]
+[assembly: AssemblyVersion("1.0.0328.0")]
+[assembly: AssemblyFileVersion("1.0.0328")]
+[assembly: AssemblyCompany("Ontica")]
+[assembly: AssemblyProduct("Empiria OnePoint 2018")]
+[assembly: AssemblyCopyright("(c) 2017-2018. La Vía Óntica SC, Ontica LLC and contributors.")]
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
