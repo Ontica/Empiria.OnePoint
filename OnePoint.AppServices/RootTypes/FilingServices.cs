53ab2e8bc553fcba948375db9809860d11f0a6cc
jmcota <jmcota@ontica.org>
2018-02-05 11:01:57 -0500

    Move application services from Empiria.Land.AppServices

---
+++ OnePoint.AppServices/RootTypes/FilingServices.cs
@@ -0,0 +1,40 @@
+﻿/* Empiria OnePoint ******************************************************************************************
+*                                                                                                            *
+*  Solution : Empiria OnePoint                             System  : OnePoint Application Services           *
+*  Assembly : Empiria.OnePoint.AppServices.dll             Pattern : Application Service                     *
+*  Type     : FilingServices                               License : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Filing use cases.                                                                              *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+using Empiria.Reflection;
+
+namespace Empiria.OnePoint.AppServices {
+
+  /// <summary>Filing use cases.</summary>
+  static public class FilingServices {
+
+    #region Services
+
+    static public IFiling GetFiling(string filingUID) {
+      Assertion.AssertObject(filingUID, "filingUID");
+
+      Type type = ServiceLocator.GetFilingType();
+
+      IFiling filing = ObjectFactory.InvokeTryParseMethod<IFiling>(type, filingUID);
+
+      if (filing == null) {
+        throw new ResourceNotFoundException("Filing.NotFound",
+                                            $"No tenemos registrado ningún trámite con número '{filingUID}'.");
+      }
+
+      return filing;
+    }
+
+    #endregion Services
+
+  }  // class FilingServices
+
+}  // namespace Empiria.OnePoint.AppServices
---
+++ OnePoint.AppServices/RootTypes/ServiceLocator.cs
@@ -0,0 +1,38 @@
+﻿/* Empiria OnePoint ******************************************************************************************
+*                                                                                                            *
+*  Solution : Empiria OnePoint                             System  : OnePoint Application Services           *
+*  Assembly : Empiria.OnePoint.AppServices.dll             Pattern : Service Locator                         *
+*  Type     : ServiceLocator                               License : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Internal static class to get external services.                                                *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+using Empiria.Reflection;
+
+namespace Empiria.OnePoint.AppServices {
+
+  /// <summary>Internal static class to get external services.</summary>
+  static internal class ServiceLocator {
+
+    // ToDo: Temporarily gets the filing type as a Empiria Land transaction.
+    // Please remove this call when transactions refactoring is completed.
+    static internal Type GetFilingType() {
+      return ObjectFactory.GetType("Empiria.Land.Registration",
+                                   "Empiria.Land.Registration.Transactions.LRSTransaction");
+    }
+
+
+    // ToDo: Remove hardcoded type's configuration.
+    static internal ITreasuryConnector GetTreasuryConnector() {
+      Type type = ObjectFactory.GetType("Empiria.Land.Connectors",
+                                        "Empiria.Land.Connectors.TreasuryConnector");
+
+      return (ITreasuryConnector) ObjectFactory.CreateObject(type);
+    }
+
+
+  }  // class ServiceLocator
+
+}  // namespace Empiria.OnePoint.AppServices
