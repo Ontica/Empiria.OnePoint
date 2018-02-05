70ea64af9366ad017ff27b2a392668e07d2d0a79
jmcota <jmcota@ontica.org>
2018-02-05 11:03:33 -0500

    Move Filing-related web services from Empiria.Land.WebApi

---
+++ OnePoint.WebApi/Filings/FilingResponseModel.cs
@@ -0,0 +1,25 @@
+﻿/* Empiria OnePoint ******************************************************************************************
+*                                                                                                            *
+*  Solution : Empiria OnePoint                             System  : OnePoint Web API                        *
+*  Assembly : Empiria.OnePoint.WebApi.dll                  Pattern : Response methods                        *
+*  Type     : FilingResponseModel                          License : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Response models for IFiling objects.                                                           *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.OnePoint.WebApi {
+
+  /// <summary>Response models for IFiling objects.</summary>
+  static internal class FilingResponseModel {
+
+    static internal object ToResponse(this IFiling filing) {
+      return new {
+        uid = filing.UID
+      };
+    }
+
+  }  // class FilingResponseModel
+
+}  // namespace Empiria.OnePoint.WebApi
---
+++ OnePoint.WebApi/Filings/FilingsController.cs
@@ -0,0 +1,58 @@
+﻿/* Empiria OnePoint ******************************************************************************************
+*                                                                                                            *
+*  Solution : Empiria OnePoint                             System  : OnePoint Web API                        *
+*  Assembly : Empiria.OnePoint.WebApi.dll                  Pattern : Web Api Controller                      *
+*  Type     : FilingsController                            License : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Contains services that interacts with filings.                                                 *
+*                                                                                                            *
+********************************* Copyright (c) 2009-2018. La Vía Óntica SC, Ontica LLC and contributors.  **/
+using System;
+using System.Web.Http;
+
+// ToDo: Merge Empiria.WebApi.Models types into Empiria.WebApi namespace.
+using Empiria.WebApi;
+using Empiria.WebApi.Models;
+
+using Empiria.OnePoint.AppServices;
+
+namespace Empiria.OnePoint.WebApi {
+
+  /// <summary>Contains services that interacts with filings.</summary>
+  public class FilingsController : WebApiController {
+
+    #region GET methods
+
+    [HttpGet]
+    [Route("v2/filings/{filingUID}")]
+    public SingleObjectModel GetFiling([FromUri] string filingUID) {
+      try {
+        IFiling filing = FilingServices.GetFiling(filingUID);
+
+        return new SingleObjectModel(this.Request, filing.ToResponse());
+
+      } catch (Exception e) {
+        throw base.CreateHttpException(e);
+
+      }
+    }
+
+
+    [HttpGet]
+    [Route("v2/filings")]
+    public PagedCollectionModel GetFilingsList([FromUri] string filter,
+                                               [FromUri] string sort) {
+      try {
+        throw new NotImplementedException();
+
+      } catch (Exception e) {
+        throw base.CreateHttpException(e);
+
+      }
+    }
+
+    #endregion GET methods
+
+  }  // class FilingsController
+
+}  // namespace Empiria.OnePoint.WebApi
