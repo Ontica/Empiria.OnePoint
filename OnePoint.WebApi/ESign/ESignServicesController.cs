945a712ea1ec94d865b067b10ced27963ed6116d
jmcota <jmcota@ontica.org>
2018-02-05 23:33:41 -0500

    Add E-Sign web api interface with initial GET methods

---
+++ OnePoint.WebApi/ESign/ESignServicesController.cs
@@ -0,0 +1,75 @@
+﻿/* Empiria OnePoint ******************************************************************************************
+*                                                                                                            *
+*  Solution : Empiria OnePoint                             System  : E-Sign Services                         *
+*  Assembly : Empiria.OnePoint.WebApi.dll                  Pattern : Web Api Controller                      *
+*  Type     : ESignServicesController                      License : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Web api interface to manage document's e-signature services.                                   *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+using Empiria.WebApi;
+using Empiria.WebApi.Models;
+
+namespace Empiria.OnePoint.ESign.WebApi {
+
+  /// <summary>Services to manage document's e-signature services.</summary>
+  public class ESignServicesController : WebApiController {
+
+    #region GET methods
+
+    [HttpGet]
+    [Route("v2/e-sign/my-pending-documents")]
+    public async Task<CollectionModel> GetDocumentsToSign([FromUri] string filter = "",
+                                                          [FromUri] string sort = "") {
+      try {
+        FixedList<SignableDocument> documents = await ESignServices.GetMyDocumentsToSign(filter, sort);
+
+        return new CollectionModel(this.Request, documents.ToResponse());
+
+      } catch (Exception e) {
+        throw base.CreateHttpException(e);
+
+      }
+    }
+
+
+    [HttpGet]
+    [Route("v2/e-sign/my-refused-documents")]
+    public async Task<CollectionModel> GetRefusedDocuments([FromUri] string filter = "",
+                                                           [FromUri] string sort = "") {
+      try {
+        FixedList<SignableDocument> documents = await ESignServices.GetMyRefusedToSignDocuments(filter, sort);
+
+        return new CollectionModel(this.Request, documents.ToResponse());
+
+      } catch (Exception e) {
+        throw base.CreateHttpException(e);
+
+      }
+    }
+
+
+    [HttpGet]
+    [Route("v2/e-sign/my-signed-documents")]
+    public async Task<CollectionModel> GetSignedDocuments([FromUri] string filter = "",
+                                                          [FromUri] string sort = "") {
+      try {
+        FixedList<SignableDocument> documents = await ESignServices.GetMySignedDocuments(filter, sort);
+
+        return new CollectionModel(this.Request, documents.ToResponse());
+
+      } catch (Exception e) {
+        throw base.CreateHttpException(e);
+
+      }
+    }
+
+    #endregion GET methods
+
+  }  // class ESignServicesController
+
+}  // namespace Empiria.OnePoint.WebApi
---
+++ OnePoint.WebApi/ESign/ESignedDocumentResponseModel.cs
@@ -0,0 +1,44 @@
+﻿/* Empiria OnePoint ******************************************************************************************
+*                                                                                                            *
+*  Solution : Empiria OnePoint                             System  : E-Sign Services                         *
+*  Assembly : Empiria.OnePoint.WebApi.dll                  Pattern : Response methods                        *
+*  Type     : ESignedDocumentResponseModel                 License : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Response models for E-signed documents.                                                        *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Empiria.OnePoint.ESign.WebApi {
+
+  /// <summary>Response models for E-signed documents.</summary>
+  static internal class ESignedDocumentResponseModel {
+
+    static internal ICollection ToResponse(this IList<SignableDocument> list) {
+      ArrayList array = new ArrayList(list.Count);
+
+      foreach (var document in list) {
+        var item = document.ToResponse();
+
+        array.Add(item);
+      }
+      return array;
+    }
+
+    static internal object ToResponse(this SignableDocument document) {
+      return new {
+        uid = document.UID,
+        type = document.DocumentType,
+        description = document.Description,
+        requestedBy = document.RequestedBy,
+        requestedTime = document.RequestedTime,
+        postedBy = document.PostedBy.Alias,
+      };
+    }
+
+  }  // class ESignedDocumentResponseModel
+
+}  // namespace Empiria.OnePoint.WebApi
+
