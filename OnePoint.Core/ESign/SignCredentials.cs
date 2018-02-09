e7d4d4a105d30e5355148912d3adf048349ee46e
jmcota <jmcota@ontica.org>
2018-02-09 11:38:48 -0500

    Signer must perform one or more e-sign commands given a SignTask

---
+++ OnePoint.Core/ESign/SignCredentials.cs
@@ -0,0 +1,81 @@
+﻿/* Empiria OnePoint ******************************************************************************************
+*                                                                                                            *
+*  Solution : Empiria OnePoint                             System  : E-Sign Services                         *
+*  Assembly : Empiria.OnePoint.dll                         Pattern : Information Holder                      *
+*  Type     : SignCredentials                              License : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Holds information about a user's electronic sign credentials.                                  *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+using Empiria.Json;
+
+namespace Empiria.OnePoint.ESign {
+
+  /// <summary>Holds information about a user's electronic sign credentials.</summary>
+  public class SignCredentials {
+
+    #region Constructors and parsers
+
+    private SignCredentials() {
+
+    }
+
+
+    private SignCredentials(JsonObject json) {
+      this.Password = json.Get<string>("password");
+    }
+
+
+    static public SignCredentials Parse(JsonObject json) {
+      SignCredentials.EnsureIsValid(json);
+
+      return new SignCredentials(json);
+    }
+
+
+    static public SignCredentials Empty {
+      get {
+        return new SignCredentials() {
+          IsEmptyInstance = true
+        };
+      }
+    }
+
+
+    static internal void EnsureIsValid(JsonObject json) {
+      Assertion.AssertObject(json, "json");
+
+      Assertion.Assert(json.HasValue("password"),
+                       "Credentials object must have a 'password' field with no empty data.");
+    }
+
+    #endregion Constructors and parsers
+
+    #region Properties
+
+    public bool IsEmptyInstance {
+      get;
+      private set;
+    } = false;
+
+
+    public string Password {
+      get;
+    }
+
+
+    public static SignCredentials ForCurrentUser() {
+      var json = new JsonObject() {
+        new JsonItem("password", "prueba"),
+      };
+
+      return new SignCredentials(json);
+    }
+
+    #endregion Properties
+
+  }  // class SignCredentials
+
+} // namespace Empiria.OnePoint.ESign
---
+++ OnePoint.Core/ESign/Signer.cs
@@ -0,0 +1,79 @@
+﻿/* Empiria OnePoint ******************************************************************************************
+*                                                                                                            *
+*  Solution : Empiria OnePoint                             System  : E-Sign Services                         *
+*  Assembly : Empiria.OnePoint.dll                         Pattern : Information Holder                      *
+*  Type     : Signer                                       License : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Performs the electronic sign of one or more documents according to a SignTask command.         *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Collections.Generic;
+
+using Empiria.Security;
+
+namespace Empiria.OnePoint.ESign {
+
+  /// <summary>Performs the electronic sign of one or more documents according
+  /// to a SignTask command.</summary>
+  public class Signer {
+
+    #region Fields
+
+    private SignCredentials credentials;
+
+    #endregion Fields
+
+    #region Constructors and parsers
+
+    public Signer(SignCredentials credentials) {
+      this.credentials = credentials;
+    }
+
+    #endregion Constructors and parsers
+
+    #region Methods
+
+    public FixedList<SignEvent> Execute(SignTask task) {
+      Assertion.AssertObject(task, "task");
+
+      this.EnsureValidCredentials(task);
+
+      List<SignEvent> eventsList = new List<SignEvent>(task.SignRequests.Count);
+
+      foreach (var request in task.SignRequests) {
+        SignEvent signEvent = this.CreateSignEvent(task, request);
+
+        eventsList.Add(signEvent);
+      }
+
+      return eventsList.ToFixedList();
+    }
+
+    #endregion Methods
+
+    #region Private methods
+
+    private SignEvent CreateSignEvent(SignTask signTask, SignRequest signRequest) {
+      string sign = this.SignInputData(signTask.ESignCredentials,
+                                       signRequest.Document.SignInputData);
+
+      var signEvent = new SignEvent(signTask.EventType, signRequest, sign);
+
+      return signEvent;
+    }
+
+    private void EnsureValidCredentials(SignTask signTask) {
+      Assertion.Assert(signTask.ESignCredentials.Password == "prueba",
+                       "No reconozco la contraseña para ejecutar la firma electrónica.");
+    }
+
+    private string SignInputData(SignCredentials credentials, string inputData) {
+      return Cryptographer.CreateHashCode(inputData, credentials.Password);
+    }
+
+    #endregion Methods
+
+  }  // class Signer
+
+}  // namespace Empiria.OnePoint.ESign
