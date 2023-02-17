424b9a5b3ced2ae1907758b5c0c0f6ab8897e373
José Manuel Cota <jmcota@ontica.org>
2023-02-16 20:45:46 -0500

    AuthenticationService and PermissionService were implemented

---
+++ Security/Services/AuthenticationService.cs
@@ -0,0 +1,77 @@
+﻿/* Empiria OnePoint ******************************************************************************************
+*                                                                                                            *
+*  Module   : Security Items                               Component : Services                              *
+*  Assembly : Empiria.OnePoint.Security.dll                Pattern   : Service provider                      *
+*  Type     : AuthenticationService                        License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Provides user authentication services.                                                         *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+using Empiria.Security;
+using Empiria.Security.Providers;
+
+namespace Empiria.OnePoint.Security.Services {
+
+  /// <summary>Provides user authentication services.</summary>
+  public class AuthenticationService: IAuthenticationProvider {
+
+    public ISubjectClaim Authenticate(ClientApplication app, string username,
+                                      string password, string entropy) {
+      Assertion.Require(app, nameof(app));
+      Assertion.Require(username, nameof(username));
+      Assertion.Require(password, nameof(password));
+      Assertion.Require(entropy, nameof(entropy));
+
+      var claim = Claim.TryParseWithKey(SecurityItemType.SubjectCredentials, app, username);
+
+      // No user found
+      if (claim == null) {
+        throw new SecurityException(SecurityException.Msg.InvalidUserCredentials);
+      }
+
+      bool useSecurityModelV3 = ConfigurationData.Get("UseSecurityModel.V3", false);
+
+      var storedPassword = claim.GetAttribute<string>("password");
+
+      string p;
+
+      if (useSecurityModelV3) {
+        p = Cryptographer.Decrypt(storedPassword, username);
+        p = Cryptographer.GetSHA256(p + entropy);
+
+      } else if (!String.IsNullOrWhiteSpace(entropy)) {
+        p = FormerCryptographer.Decrypt(storedPassword, username);
+        p = FormerCryptographer.GetMD5HashCode(p + entropy);
+
+      } else {
+        p = FormerCryptographer.Decrypt(storedPassword, username);
+      }
+
+      //Invalid password
+      if (p != password) {
+        throw new SecurityException(SecurityException.Msg.InvalidUserCredentials);
+      }
+
+      return claim;
+    }
+
+
+    public ISubjectClaim TryGetUser(ClientApplication app, int userId) {
+      Assertion.Require(app, nameof(app));
+
+      return Claim.TryParse(SecurityItemType.SubjectCredentials, app, userId);
+    }
+
+
+    public ISubjectClaim TryGetUserWithUserName(ClientApplication app, string username) {
+      Assertion.Require(app, nameof(app));
+      Assertion.Require(username, nameof(username));
+
+      return Claim.TryParseWithKey(SecurityItemType.SubjectCredentials, app, username);
+    }
+
+  }  // class AuthenticationService
+
+}  // namespace Empiria.OnePoint.Security.Services
---
+++ Security/Services/PermissionsService.cs
@@ -0,0 +1,92 @@
+﻿/* Empiria OnePoint ******************************************************************************************
+*                                                                                                            *
+*  Module   : Security Items                               Component : Services                              *
+*  Assembly : Empiria.OnePoint.Security.dll                Pattern   : Service provider                      *
+*  Type     : PermissionsService                           License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Provides subject permissions read services.                                                    *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+using Empiria.Security;
+using Empiria.Security.Providers;
+
+namespace Empiria.OnePoint.Security.Services {
+
+  /// <summary>Provides subject permissions read services.</summary>
+  public class PermissionsService : IPermissionsProvider {
+
+    public FixedList<string> GetFeaturesPermissions(ClientApplication app, EmpiriaIdentity subject) {
+      Assertion.Require(app, nameof(app));
+      Assertion.Require(subject, nameof(subject));
+
+      var permissionsBuilder = new PermissionsBuilder(app, subject);
+
+      return permissionsBuilder.BuildFeatures()
+                               .Select(x => x.Key)
+                               .ToFixedList();
+    }
+
+
+    public FixedList<IObjectAccessRule> GetObjectAccessRules(ClientApplication app, EmpiriaIdentity subject) {
+      Assertion.Require(app, nameof(app));
+      Assertion.Require(subject, nameof(subject));
+
+      var permissionsBuilder = new PermissionsBuilder(app, subject);
+
+      return permissionsBuilder.BuildObjectAccessRules()
+                               .Select(x => MapToObjectAccessRulesDto(x))
+                               .ToFixedList();
+    }
+
+
+    public FixedList<string> GetRoles(ClientApplication app, EmpiriaIdentity subject) {
+      Assertion.Require(app, nameof(app));
+      Assertion.Require(subject, nameof(subject));
+
+      var permissionsBuilder = new PermissionsBuilder(app, subject);
+
+      return permissionsBuilder.BuildRoles()
+                               .Select(x => x.Key)
+                               .ToFixedList();
+    }
+
+
+    public bool IsSubjectInRole(ClientApplication app, IIdentifiable subject, string role) {
+      Assertion.Require(app, nameof(app));
+      Assertion.Require(subject, nameof(subject));
+      Assertion.Require(role, nameof(role));
+
+      return Role.IsSubjectInRole(app, subject, role);
+    }
+
+
+    private IObjectAccessRule MapToObjectAccessRulesDto(ObjectAccessRule rule) {
+      return new ObjectAccessRuleDto {
+        TypeName = rule.TypeName,
+        ObjectsUIDs = rule.ObjectsUIDs.ToArray()
+      };
+    }
+
+
+  }  // class AuthenticationService
+
+
+
+  internal class ObjectAccessRuleDto : IObjectAccessRule {
+
+    public string TypeName {
+      get;
+      internal set;
+    }
+
+
+    public string[] ObjectsUIDs {
+      get;
+      internal set;
+    }
+
+  }  // class ObjectAccessRuleDto
+
+}  // namespace Empiria.OnePoint.Security.Services
