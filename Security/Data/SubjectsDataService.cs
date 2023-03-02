6c9adb69af66f3c70d030c041b8452c0d6e79d81
José Manuel Cota <jmcota@ontica.org>
2023-03-02 12:20:50 -0500

    Fix Size of legacy table field MHParticipants.ParticipantKey is 24

--- Security/Data/SubjectsDataService.cs
+++ Security/Data/SubjectsDataService.cs
@@ -108,8 +108,11 @@ namespace Empiria.OnePoint.Security.Data {
         return 0;
       }
 
+      var formerUserID = userID.Length <= 24 ?
+                         userID : userID.Substring(0, 24);
+
       string sql = "SELECT ParticipantId FROM MHParticipants " +
-                  $"WHERE LOWER(ParticipantKey) = '{userID.ToLower()}'";
+                  $"WHERE LOWER(ParticipantKey) = '{formerUserID.ToLower()}'";
 
       return Convert.ToInt32(DataReader.GetScalar<long>(DataOperation.Parse(sql), 0));
     }
@@ -121,10 +124,13 @@ namespace Empiria.OnePoint.Security.Data {
       }
 
       var fullname = subject.Contact.FullName.Length <= 64 ?
-                     subject.Contact.FullName : subject.Contact.FullName.Substring(0, 64);
+                          subject.Contact.FullName : subject.Contact.FullName.Substring(0, 64);
+
+      var userID = subject.UserID.Length <= 24 ?
+                          subject.UserID : subject.UserID.Substring(0, 24);
 
       var op = DataOperation.Parse("write_mhparticipant", subject.Contact.Id,
-          fullname, subject.UserID, subject.Contact.FullName, 'U', 'A',
+          fullname, userID, subject.Contact.FullName, 'U', 'A',
           new DateTime(2022, 01, 01), new DateTime(2049, 12, 31));
 
       DataWriter.Execute(op);
