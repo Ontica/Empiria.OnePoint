34b3a5d3b8cccedcbbf1575ab1fadb9c0aa58465
José Manuel Cota <jmcota@ontica.org>
2023-06-02 16:34:52 -0600

    Rfx WebServer changed to SoftwareSystem

--- Security/Domain/SoftwareSystem.cs
+++ Security/Domain/SoftwareSystem.cs
@@ -2,33 +2,34 @@
 *                                                                                                            *
 *  Module   : Security Items                               Component : Domain Layer                          *
 *  Assembly : Empiria.OnePoint.Security.dll                Pattern   : Information holder                    *
-*  Type     : WebServer                                    License   : Please read LICENSE.txt file          *
+*  Type     : SoftwareSystem                               License   : Please read LICENSE.txt file          *
 *                                                                                                            *
-*  Summary  : Represents a web server.                                                                       *
+*  Summary  : Represents a software system.                                                                  *
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 using System;
 
 namespace Empiria.OnePoint.Security {
 
-  /// <summary>Represents a web server.</summary>
-  internal sealed class WebServer : SecurityItem, INamedEntity {
+  /// <summary>Represents a software system.</summary>
+  internal sealed class SoftwareSystem : SecurityItem, INamedEntity {
 
-    internal WebServer(SecurityItemType powerType) : base(powerType) {
+    internal SoftwareSystem(SecurityItemType powerType) : base(powerType) {
       // Required by Empiria Framework for all partitioned types.
     }
 
-    static public new WebServer Parse(int id) {
-      return BaseObject.ParseId<WebServer>(id);
+    static public new SoftwareSystem Parse(int id) {
+      return BaseObject.ParseId<SoftwareSystem>(id);
     }
 
 
     public string Name {
       get {
-        return ExtensionData.Get("serverName", base.BaseKey);
+        return ExtensionData.Get("systemName", base.BaseKey);
       }
     }
 
-  } // class WebServer
+
+  } // class SoftwareSystem
 
 } // namespace Empiria.OnePoint.Security
