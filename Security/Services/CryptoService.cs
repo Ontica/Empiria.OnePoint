e3fc089d2599721bc15ffb869ed0c77ecaa94080
José Manuel Cota <jmcota@ontica.org>
2023-06-10 12:01:59 -0600

    Rfx RSAProvider moved from Empiria Core https://rb.gy/07qsg

---
+++ Security/Services/CryptoService.cs
@@ -0,0 +1,36 @@
+﻿/* Empiria OnePoint ******************************************************************************************
+*                                                                                                            *
+*  Module   : Security                                     Component : Authorization services                *
+*  Assembly : Empiria.OnePoint.Security.dll                Pattern   : Service provider                      *
+*  Type     : CryptoService                                License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Provides subject's authorization services.                                                     *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+using Empiria.Security.Providers;
+
+using System.Security.Cryptography;
+using System.Security;
+
+namespace Empiria.OnePoint.Security.Services {
+
+  /// <summary>Provides subject's authorization services.</summary>
+  public class CryptoService : ICryptoServiceProvider {
+
+    public RSACryptoServiceProvider GetRSAProvider(string privateKeyFilePath, SecureString password) {
+      Assertion.Require(privateKeyFilePath, nameof(privateKeyFilePath));
+      Assertion.Require(password, nameof(password));
+
+      return RSAProvider.GetProvider(privateKeyFilePath, password);
+    }
+
+
+    public RSACryptoServiceProvider GetRSASystemProvider() {
+      return RSAProvider.GetSystemProvider();
+    }
+
+  }  // class CryptoService
+
+}  // namespace Empiria.OnePoint.Security.Services
