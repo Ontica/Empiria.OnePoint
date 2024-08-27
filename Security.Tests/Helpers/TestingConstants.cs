b3f799be9a53fc525025e151421f66007cdf0c42
José Manuel Cota <jmcota@ontica.org>
2024-08-27 10:55:52 -0400

    Activate and suspend subjects use cases tests added

---
+++ Security.Tests/Helpers/TestingConstants.cs
@@ -0,0 +1,20 @@
+﻿/* Empiria OnePoint  *****************************************************************************************
+*                                                                                                            *
+*  Module   : One Point Security Services                  Component : Test cases                            *
+*  Assembly : Empiria.OnePoint.Security.Tests.dll          Pattern   : Testing constants                     *
+*  Type     : TestingConstants                             License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Provides testing constants.                                                                    *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.OnePoint.Security.Tests {
+
+  /// <summary>Provides testing constants.</summary>
+  static public class TestingConstants {
+
+    static internal string SUBJECT_UID => ConfigurationData.GetString("SUBJECT_UID");
+
+  }  // class TestingConstants
+
+}  // namespace Empiria.OnePoint.Security.Tests
